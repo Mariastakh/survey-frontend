f0d6bcf477c878f5ffbbd2bde8523cab2c981cb9
Mariastakh <astakhovam94@gmail.com>
2020-04-22 16:12:28 +0100

    add a test for fetch logic

--- src/Services/getTopics.jsx
+++ src/Services/getTopics.jsx
@@ -1,4 +1,6 @@
 export default async function getTopics() {
-    let response = await fetch("https://localhost:5001/api/topics").then(response => response.json());
+    let response = await fetch("https://localhost:5001/api/topics");
+    console.log(response.status);
+    
     return response
 }
---
+++ src/Test/getTopics.test.js
@@ -0,0 +1,35 @@
+import getTopics from "../Services/getTopics";
+
+describe('getTopic', () => {
+  
+  beforeAll(() => {
+    global.fetch = jest.fn();
+    //window.fetch = jest.fn(); if running browser environment
+  });
+
+    beforeEach(() => {
+     
+  
+    });
+  
+    it('shows surveys after hitting the api endpoint', async () => {
+
+      fetch.mockImplementation(() => {
+        return Promise.resolve({
+          status: 200,
+          json: () => {
+          return Promise.resolve({
+             Topic: "One"
+           });
+         }
+       });
+     });
+
+      let response = await getTopics();
+      expect(response.status).toBe(200);
+      
+    });
+  
+    
+  
+  });
