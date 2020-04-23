99b6cf8ff96281d80f0ea989c5652ce1afc62051
Mariastakh <astakhovam94@gmail.com>
2020-04-23 10:48:36 +0100

    Update component with error handling logic following react tutorial at https://reactjs.org/docs/faq-ajax.html

--- src/Components/DisplayTopics.jsx
+++ src/Components/DisplayTopics.jsx
@@ -5,6 +5,8 @@ export default class DisplayTopics extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      error: null,
+      isLoaded: false,
       topics: []
     };
   }
@@ -16,11 +18,13 @@ export default class DisplayTopics extends Component {
       (result) => {
           console.log(result);
         this.setState({
+          isLoaded: true,
           topics: result
         });
       },
       (error) => {
         this.setState({
+          isLoaded: true,
          error
         });
       }
@@ -28,16 +32,24 @@ export default class DisplayTopics extends Component {
   }
 
   render(){
-    let topics = (
-        <>
-            <div>
-              <h2>Welcome</h2>
-              {this.state.topics.map(item => (
+    const { error, isLoaded, topics } = this.state;
+    if (error)
+    {
+      return <div>Error: { error.message } </div>
+    } else if (!isLoaded){
+      return <div>Loading...</div>
+    } else {
+    let t = (
+      <>
+      <h2>Surveys</h2>
+       <ul>
+            {topics.map(item => (
             <li key={item}>{item}</li>
           ))}
-          </div>
+          </ul>
          </>
     ); 
-      return topics;
+      return t;
+              }
   }
 }
