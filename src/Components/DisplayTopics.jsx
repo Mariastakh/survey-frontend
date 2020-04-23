ad6edada083d3e6bdeaff80af49888f45c032594
Mariastakh <astakhovam94@gmail.com>
2020-04-23 11:09:31 +0100

    make topics into buttons

--- src/Components/DisplayTopics.jsx
+++ src/Components/DisplayTopics.jsx
@@ -7,49 +7,48 @@ export default class DisplayTopics extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      topics: []
+      topics: [],
     };
   }
 
   async componentDidMount() {
     fetch("https://localhost:5001/api/topics")
-    .then(res => res.json())
-    .then(
-      (result) => {
+      .then((res) => res.json())
+      .then(
+        (result) => {
           console.log(result);
-        this.setState({
-          isLoaded: true,
-          topics: result
-        });
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-         error
-        });
-      }
-    )
+          this.setState({
+            isLoaded: true,
+            topics: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
   }
 
-  render(){
+  render() {
     const { error, isLoaded, topics } = this.state;
-    if (error)
-    {
-      return <div>Error: { error.message } </div>
-    } else if (!isLoaded){
-      return <div>Loading...</div>
+    if (error) {
+      return <div>Error: {error.message} </div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
     } else {
-    let listOfTopics = (
-      <>
-      <h2>Surveys</h2>
-       <ul>
-            {topics.map(item => (
-            <li key={item}>{item}</li>
-          ))}
+      let listOfTopics = (
+        <>
+          <h2>Surveys</h2>
+          <ul>
+            {topics.map((item) => (
+              <button>{item}</button>
+            ))}
           </ul>
-         </>
-    ); 
+        </>
+      );
       return listOfTopics;
-              }
+    }
   }
 }
